b7c490f53463d25df872f4b2cfa9ccde0d518983
Daemon <makushin.dima@gmail.com>
2019-10-19 13:34:42 +0300

    up: gitignore

--- product-reviews/src/app/pages/detail-product/detail-product.component.ts
+++ product-reviews/src/app/pages/detail-product/detail-product.component.ts
@@ -5,36 +5,36 @@ import { ApiService } from 'src/app/services/api.service';
 import { DatakeepService } from 'src/app/services/datakeep.service';
 
 @Component({
-  selector: 'app-detail-product',
-  templateUrl: './detail-product.component.html',
-  styleUrls: ['./detail-product.component.scss']
+	selector: 'app-detail-product',
+	templateUrl: './detail-product.component.html',
+	styleUrls: ['./detail-product.component.scss']
 })
 export class DetailProductComponent implements OnInit {
 
-  id: any;
-  product: any;
-  reviews: any;
+	id: any;
+	product: any;
+	reviews: any;
 
-  constructor(
-    public api: ApiService,
-    private route: ActivatedRoute,
-    private location: Location,
-    public data: DatakeepService
-  ) {
-    console.log('id:::', this.route.snapshot.paramMap.get('id'));
-  }
+	constructor(
+		public  api      : ApiService,
+		private route    : ActivatedRoute,
+		private location : Location,
+		public  data     : DatakeepService
+	) {
+		console.log('id:::', this.route.snapshot.paramMap.get('id'));
+	}
 
-  ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.api.listReviews(this.id).subscribe(
-      data => { this.reviews = data },
-      error => { console.log('error: ', error) }
-    );
-    this.product = this.data.check_product;
-  }
+	ngOnInit() {
+		this.id = this.route.snapshot.paramMap.get('id');
+		this.api.listReviews(this.id).subscribe(
+			data => { this.reviews = data },
+			error => { console.log('error: ', error) }
+		);
+		this.product = this.data.check_product;
+	}
 
-  goBack() {
-    this.location.back();
-  }
+	goBack() {
+		this.location.back();
+	}
 
 }
--- product-reviews/src/app/services/datakeep.service.ts
+++ product-reviews/src/app/services/datakeep.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DatakeepService {
 
-  check_product: any;
+	check_product: any;
 
-  constructor() { }
+	constructor() { }
 }
